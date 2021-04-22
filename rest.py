e0eefb8e6c152c69e81fd280c6b3f7e219afa63b
Dipaditya Das <DipadityaDas@gmail.com>
2021-04-22 23:08:37 +0530

    Chnages rest api python file

--- rest.py
+++ rest.py
@@ -5,43 +5,43 @@ from bs4 import BeautifulSoup as bs
 
 
 def weather_data(x):
-	r = get(
-		"https://www.weather-forecast.com/locations/"+x+"/forecasts/latest")
+    r = get(
+        "https://www.weather-forecast.com/locations/"+x+"/forecasts/latest")
 
-	forecast = bs(r.content, "lxml")
-	latitude = forecast.find("span", {"class": "latitude"}).text
-	longitude = forecast.find("span", {"class": "longitude"}).text
-	day_name = datetime.now().strftime("%a")
-	day_date = datetime.now().strftime("%d")
-	temp = forecast.find_all("span", {"class": "temp b-forecast__table-value"})
-	max_temp = temp[0].text
-	min_temp = temp[36].text
-	prediction = forecast.find("span", {"class": "phrase"}).text
+    forecast = bs(r.content, "lxml")
+    latitude = forecast.find("span", {"class": "latitude"}).text
+    longitude = forecast.find("span", {"class": "longitude"}).text
+    day_name = datetime.now().strftime("%a")
+    day_date = datetime.now().strftime("%d")
+    temp = forecast.find_all("span", {"class": "temp b-forecast__table-value"})
+    max_temp = temp[0].text
+    min_temp = temp[36].text
+    prediction = forecast.find("span", {"class": "phrase"}).text
 
-	y = dict(loads(get("http://api.openweathermap.org/data/2.5/weather?q=" +
-               str(x).capitalize()+"&appid=3881348223779479ede078d9c8a5e2b0").content))
-	pressure = str(y['main']['pressure'])
-	humidity = str(y['main']['humidity'])
-	wind = str(round(y['wind']['speed'] * 3.6, 3))
+    y = dict(loads(get("http://api.openweathermap.org/data/2.5/weather?q=" +
+                       str(x).capitalize()+"&appid=3881348223779479ede078d9c8a5e2b0").content))
+    pressure = str(y['main']['pressure'])
+    humidity = str(y['main']['humidity'])
+    wind = str(round(y['wind']['speed'] * 3.6, 3))
 
-	if float(wind) > 100.0:
-		cyclone = "Yes"
-	else:
-		cyclone = "No"
+    if float(wind) > 100.0:
+        cyclone = "Yes"
+    else:
+        cyclone = "No"
 
-	return {"city": str(x).capitalize(),
-         "latitude": latitude,
-         "longitude": longitude,
-         "day_date": day_date,
-         "day_name": day_name,
-         "max_temp": max_temp,
-         "min_temp": min_temp,
-         "humidity": humidity,
-		 "pressure": pressure,
-         "wind": wind,
-         "prediction": prediction,
-		 "cyclone": cyclone}
-	# print(data)
+    return {"city": str(x).capitalize(),
+            "latitude": latitude,
+            "longitude": longitude,
+            "day_date": day_date,
+            "day_name": day_name,
+            "max_temp": max_temp,
+            "min_temp": min_temp,
+            "humidity": humidity,
+            "pressure": pressure,
+            "wind": wind,
+            "prediction": prediction,
+            "cyclone": cyclone}
+    # print(data)
 
 
 # if __name__ == "__main__":
