987f333ba301081a63928ba44460727b4de67c2f
Dipaditya Das <DipadityaDas@gmail.com>
2021-04-22 23:06:07 +0530

    Chnages app.py

--- app.py
+++ app.py
@@ -9,34 +9,36 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 @app.route("/")
 @app.route("/index")
 def home():
-	return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/project")
 def project():
-	return render_template("project.html")
+    return render_template("project.html")
 
 
 @app.route("/location", methods=['GET'])
 def location():
-	x = request.args.get("x")
-	r = get(
-		"https://www.weather-forecast.com/locations/"+x+"/forecasts/latest")
+    x = request.args.get("x")
+    r = get(
+        "https://www.weather-forecast.com/locations/"+x+"/forecasts/latest")
 
-	if r.status_code != 200:
-		return render_template("notfound.html")
+    if r.status_code != 200:
+        return render_template("notfound.html")
 
-	args = weather_data(x)
-	return render_template("location.html", data=args)
+    args = weather_data(x)
+    return render_template("location.html", data=args)
 
 
 @app.route("/contact")
 def contact():
-	return render_template("contact.html")
+    return render_template("contact.html")
 
 
 @app.route("/about")
 def about():
-	return render_template("about.html")
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0", port=5000)
